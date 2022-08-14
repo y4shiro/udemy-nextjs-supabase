ab52dd90a7020896509f60a88fb5271dcb793606
y4shiro <y4shironao@gmail.com>
2022-08-14 21:57:22 +0900

    add: ssr ページを作成

--- project1/pages/ssr.tsx
+++ project1/pages/ssr.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 
 import { Layout } from '../components/Layout'
 import { supabase } from '../utils/supabase'
 import { Task, Notice } from '../types/types'
 
-export const getStaticProps: GetStaticProps = async () => {
-  console.log('getStaticProps/ssg invoked')
+export const getServerSideProps: GetServerSideProps = async () => {
+  console.log('getServerSideProps/ssr invoked')
 
   const { data: tasks } = await supabase
     .from('todos')
@@ -28,12 +28,13 @@ type StaticProps = {
   notices: Notice[]
 }
 
-const Ssg: NextPage<StaticProps> = ({ tasks, notices }) => {
+export const Ssr: NextPage<StaticProps> = ({ tasks, notices }) => {
   const router = useRouter()
 
   return (
-    <Layout title="SSG">
-      <p className="mb-3 text-blue-500">SSG</p>
+    <Layout title="SSR">
+      <p className="mb-3 text-pink-500">SSR</p>
+
       <ul className="mb-3">
         {tasks.map((task) => (
           <li key={task.id}>
@@ -50,15 +51,15 @@ const Ssg: NextPage<StaticProps> = ({ tasks, notices }) => {
         ))}
       </ul>
 
-      <Link href="/ssr" prefetch={false}>
-        <a className="my-3 text-xs">Link to ssr</a>
+      <Link href="/ssg" prefetch={false}>
+        <a className="my-3 text-xs">Link to ssg</a>
       </Link>
       <Link href="/isr" prefetch={false}>
         <a className="mb-3 text-xs">Link to isr</a>
       </Link>
 
-      <button className="mb-3 text-xs" onClick={() => router.push('/ssr')}>
-        Route to ssr
+      <button className="mb-3 text-xs" onClick={() => router.push('/ssg')}>
+        Route to ssg
       </button>
       <button className="mb-3 text-xs" onClick={() => router.push('/isr')}>
         Route to isr
@@ -67,4 +68,4 @@ const Ssg: NextPage<StaticProps> = ({ tasks, notices }) => {
   )
 }
 
-export default Ssg
+export default Ssr
