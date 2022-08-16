68c944cc5a52ff9b7b7f48f964c6e055a05dc2a4
y4shiro <y4shironao@gmail.com>
2022-08-16 11:10:25 +0900

    add: Task のフォーム / リストコンポーネントを実装

--- project1/pages/dashboard.tsx
+++ project1/pages/dashboard.tsx
@@ -1,8 +1,14 @@
 import { NextPage } from 'next'
-import { LogoutIcon } from '@heroicons/react/solid'
+import {
+  LogoutIcon,
+  StatusOnlineIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/solid'
 
 import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
+import { TaskForm } from '../components/TaskForm'
+import { TaskList } from '../components/TaskList'
 
 const Dashboard: NextPage = () => {
   const signOut = () => {
@@ -15,6 +21,16 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
     </Layout>
   )
 }
