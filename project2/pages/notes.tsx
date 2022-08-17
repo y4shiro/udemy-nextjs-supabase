2f56affc26bf716b6aa9409c02fbb0655afed531
y4shiro <y4shironao@gmail.com>
2022-08-17 18:46:58 +0900

    add: Note のフォームとリストを実装

--- project2/pages/notes.tsx
+++ project2/pages/notes.tsx
@@ -1,9 +1,30 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import { LogoutIcon, DocumentTextIcon } from '@heroicons/react/solid'
+
 import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
 
-const Notes: NextPage = () => {
+import { NoteForm } from '../components/NoteForm'
+import { NoteItem } from '../components/NoteItem'
+import type { Note } from '../types/types'
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log('ISR invoked - notes page')
+  const { data: notes, error } = await supabase
+    .from('notes')
+    .select('*')
+    .order('created_at', { ascending: true })
+
+  if (error) throw new Error(`${error.message}: ${error.details}`)
+
+  return { props: { notes }, revalidate: false }
+}
+
+type StaticProps = {
+  notes: Note[]
+}
+
+const Notes: NextPage<StaticProps> = ({ notes }) => {
   const signOut = () => {
     supabase.auth.signOut()
   }
@@ -14,6 +35,21 @@ const Notes: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <DocumentTextIcon className="h-8 w-8 text-blue-500" />
+
+      <ul className="my-2">
+        {notes.map((note) => (
+          <NoteItem
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            content={note.content}
+            user_id={note.user_id}
+          />
+        ))}
+      </ul>
+
+      <NoteForm />
     </Layout>
   )
 }
